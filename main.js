f03f8d0dd917991c9882fd372d20530c51272039
Nilesh217 <nileshdwivedi2006@gmail.com>
2024-11-22 23:45:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Coder", "Creater", "Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
